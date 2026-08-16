9a7bc1baad37703b0717f2f086d2d083460c8724
DavidLee36 <david303lee@gmail.com>
2026-08-15 19:08:10 -0700

    populate moves vector

--- robot/MicroController/src/main.cpp
+++ robot/MicroController/src/main.cpp
@@ -3,34 +3,85 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
-#include "secrets.h"   // gitignored; defines WIFI_SSID / WIFI_PASSWORD
+#include <vector>
+#include "secrets.h" // gitignored; defines WIFI_SSID / WIFI_PASSWORD
 
 WebServer server(80);
 
 String currStatus = "IDLE";
 
-void send(int code, const String& body) {
+// Storing moves as strings because ex. move 02 wouldn't work as an int
+std::vector<String> movesVector;
+
+/// @brief Populate movesVector from string provided by Svelte,
+/// moves are transmitted with no seperator, ex. 023143...
+/// @param movesStr 
+void populateMoves(String movesStr)
+{
+	movesVector.clear();
+	int idx = 0;
+	while (idx < movesStr.length() - 2)
+	{
+		// Ensure move is valid
+		try
+		{
+			int motor = movesStr.charAt(idx) - '0';
+			int dir = movesStr.charAt(idx + 1) - '0';
+			if (motor > 5 || motor < 0 || dir > 3 || dir < 0)
+			{
+				throw new std::exception;
+			}
+		}
+		catch (const std::exception &e)
+		{
+			// Do not automatically clear and reset the moves list, we want to make sure Svelte is
+			// aware on the next /status call
+			currStatus = "ERROR invalid move detected";
+			return;
+		}
+
+		// push move to movesVector
+		movesVector.push_back(movesStr.substring(idx, idx+2));
+		idx++;
+	}
+	currStatus = "POPULATED MOVES " + movesVector.size();
+}
+
+void send(int code, const String &body)
+{
 	server.sendHeader("Access-Control-Allow-Origin", "*");
 	server.send(code, "text/plain", body);
 }
 
-void statusResponse() {
+void statusResponse()
+{
 	send(200, currStatus);
 }
 
-void movesResponse() {
+void movesResponse()
+{
 	String body = server.arg("plain");
 	Serial.print("got moves: ");
 	Serial.println(body);
-	send(202, "accepted");
-	currStatus = "PROCESSING MOVES";
+	if (currStatus.equals("IDLE"))
+	{ // only accept a new move list when robot is idle
+		send(202, "accepted");
+		currStatus = "PROCESSING MOVES";
+		populateMoves(body);
+	}
+	else
+	{
+		send(205, "not accepting moves at this time");
+	}
 }
 
-void setupWIFI() {
+void setupWIFI()
+{
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 	Serial.print("Connecting");
-	while (WiFi.status() != WL_CONNECTED) {
+	while (WiFi.status() != WL_CONNECTED)
+	{
 		delay(300);
 		Serial.print(".");
 	}
@@ -40,22 +91,25 @@ void setupWIFI() {
 
 	WiFi.setSleep(false);
 
-	if (MDNS.begin("cube")) {
+	if (MDNS.begin("cube"))
+	{
 		Serial.println("mDNS: cube.local");
 	}
-	server.on("/status", HTTP_GET,  statusResponse);
-	server.on("/moves",  HTTP_POST, movesResponse);
+	server.on("/status", HTTP_GET, statusResponse);
+	server.on("/moves", HTTP_POST, movesResponse);
 	server.begin();
 	Serial.println("server up");
 }
 
-void setup() {
+void setup()
+{
 	Serial.begin(115200);
 	delay(200);
 	setupWIFI();
 }
 
-void loop() {
+void loop()
+{
 	server.handleClient();
 	delay(2);
 }
